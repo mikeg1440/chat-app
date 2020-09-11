8ad67ef286edebb16c69649710605af2724ee236
Michael Gaudreau <gaudreaum01@gmail.com>
2020-09-11 14:45:52 -0400

    Add userJoin and getCurrentUser methods to the new user.js file

--- utils/user.js
+++ utils/user.js
@@ -1,1 +1,19 @@
+
 const users = [];
+
+module.exports = {
+  userJoin,
+  getCurrentUser
+}
+
+function userJoin(id, username, room){
+  const user = {id, username, room};
+
+  users.push(user);
+
+  return user;
+}
+
+function getCurrentUser(id){
+  return users.find(user => user.id === id);
+}
