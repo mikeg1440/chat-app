205ab558aace0936d6cab4929429df24e8ac058b
Michael Gaudreau <gaudreaum01@gmail.com>
2020-09-11 14:06:51 -0400

    Add constants for running server and sockets

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/message');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
