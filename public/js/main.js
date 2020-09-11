f7d9eaa891e77f3cbde382692f32953675ed5371
Michael Gaudreau <gaudreaum01@gmail.com>
2020-09-11 14:04:35 -0400

    Add action for handling a 'message' request from server

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
 
 const chatBox = document.getElementById('chatBox');
 const chatForm = document.getElementById('chatForm');
+const socket = io();
+socket.on('message', handleMessage);
+
+function handleMessage(msg){
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${msg.username} <span>${msg.time}</span></p>
+  <p class="text">
+    ${msg.text}
+  </p>`
+  chatBox.appendChild(div);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
