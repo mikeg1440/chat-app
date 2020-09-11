44beb5a1fb2f05a347e654962ed85acb0982a275
Michael Gaudreau <gaudreaum01@gmail.com>
2020-09-11 14:47:37 -0400

    Add import for new userJoin and getCurrentUser methods in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/message');
+const {userJoin, getCurrentUser} = require('./utils/user');
 
 const app = express();
 const server = http.createServer(app);
